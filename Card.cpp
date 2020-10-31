e043769473790008d4253b6eb7fc7dc678ed8144
ehopkins2 <erica.hopkins008@gmail.com>
2020-10-31 16:39:39 -0500

    Minor edits to comments.

--- Card.cpp
+++ Card.cpp
@@ -3,7 +3,7 @@
 //default constructor
 Card::Card():moveSpaces(0) {}
 
-//constructor that takes in a value for member variable
+//constructor that takes in a value for each member variable
 Card::Card(unsigned int n, string str1, string str2, string str3)
  : moveSpaces(n), riddle(str1), answer(str2), rescueItem(str3) {}
 
