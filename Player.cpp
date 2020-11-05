d5d3ac6695dbd729491ff2f1fec2b81d642d4d62
ehopkins2 <erica.hopkins008@gmail.com>
2020-11-05 15:26:30 -0600

    Added overloaded insertion (<<) operator.

--- Player.cpp
+++ Player.cpp
@@ -1,10 +1,22 @@
 #include "Player.h"
 
+using std::endl;
+
 //default constructor
-Player::Player() : lives(3) {}
+Player::Player() : lives(0) {}
 
 //constructor that takes in player name, color and path data
 //path data consists of size of path, number of wait for 5 or 8, number of jungle and number of rhino
-//always initializes the number of lives to 3
+//always initializes the number of lives to 5
 Player::Player(const string st1, const string st2, const vector<unsigned int>& pathData) 
- : username(st1), gameColor(st2), lives(3), playerPath(pathData) {}
+ : username(st1), gameColor(st2), lives(5), playerPath(pathData) {}
+
+ostream& operator <<(ostream &os, Player player)
+{
+    os << "Player Info:" << endl;
+    os << "Name: " << player.get_username() << endl;
+    os << "Color: " << player.get_gameColor() << endl;
+    os << "Lives left: " << player.get_lives() << endl;
+    os << "Position: " << (player.get_playerPath()).get_position() << endl;
+    return os;
+}
