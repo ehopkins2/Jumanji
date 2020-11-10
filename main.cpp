a5cbf7eb2a1f6eb5e3aeb61daadc68967cca49f1
ehopkins2 <erica.hopkins008@gmail.com>
2020-11-10 16:29:02 -0600

    Moved most of the game play functionality to a different file, and added checking if the player is stuck on a rhino before rolling.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 //Game Description
 
+#include <iostream>
+#include <string>
 #include <vector>
 #include <tuple>
 #include <map>
+#include <algorithm>
 #include <time.h>
 #include "Card.h"
 #include "Deck.h"
 #include "Path.h"
 #include "Player.h"
 #include "SetupGame.h"
+#include "GamePlay.h"
 
 
-using std::cin;     using std::vector;
+using std::cin;     using std::vector;      using std::string;
 using std::cout;    using std::map;         
 using std::endl;    using std::tuple;
 
@@ -28,10 +32,88 @@ int main()
     //Setup the game
     map<string, Player> players = setupPlayers();
     Deck mainDeck = setupDeck();
-    map<string, vector<tuple<unsigned int, string>>> intersections = setupIntersections();
+    
+    bool winner = false;
+    unsigned int numPlayersAlive = 4;
+    bool exit = false;
 
-    Card nextCard = mainDeck.draw();
-    cout << nextCard;
+    while(!winner || numPlayersAlive <=1)
+    {
+        if(exit)
+        {
+            cout << "You will now exit the game." << endl;
+            break;
+        }
+
+        for(auto &p : players)
+        {
+            Player currentPlayer = p.second;
+            if(currentPlayer.get_lives() == 0)
+                break;
+
+            cout << endl;
+            cout << "________________" << endl;
+            cout << endl;
+            cout << currentPlayer.get_username() << ", it is now your turn." << endl;
+            cout << endl;
+
+            string temp;
+            cout << "At this time, you may exit the game by typing exit or you can continue by typing anything else.";
+            getline(cin, temp);
+            cout << endl;
+                
+            std::transform(temp.begin(), temp.end(), temp.begin(), [](unsigned char c){return std::tolower(c);}); //uses Lambda expression
+            if(temp == "exit")
+            {
+                exit = true;
+                break;
+            }
+
+            bool moveSuccessful = false;
+            const unsigned int numMove = rollGameDie();
+
+            if(currentPlayer.get_playerPath().get_path()[currentPlayer.get_playerPath().get_position()] != "Rhino")
+            {
+                cout << "You roll the game die." << endl;
+                cout << "You rolled a " << numMove << "." << endl;
+                moveSuccessful = currentPlayer.movePlayer("forward", numMove, players);
+            }
+            else
+            {
+                cout << "You are currently stuck on a Rhino." << endl;
+                if(currentPlayer.get_playerPath().get_position() == 40)
+                {
+                    cout << "Since you are currently at position 40, you can ignore the Rhino. You need to roll a 1 to win." << endl;
+                    cout << "You rolled a " << numMove << "." << endl;
+                    moveSuccessful = currentPlayer.movePlayer("forward", numMove, players);
+                }
+                else
+                {
+                    cout << "You roll the game die. You need to roll an even number to move forward." << endl;
+                    cout << "You rolled a " << numMove << "." << endl;
+                    if(numMove%2 == 0)
+                        moveSuccessful = currentPlayer.movePlayer("forward", numMove, players);
+                    else
+                        cout << "You did not roll an even number, so you are stuck." << endl;
+                }
+            }
+
+            if(moveSuccessful || currentPlayer.get_playerPath().get_position() == 41)
+            {
+                doActionOnPath(players, currentPlayer, mainDeck, numPlayersAlive, winner);
+                players[currentPlayer.get_gameColor()] = currentPlayer;
+            }     
+                        
+            if(winner || numPlayersAlive <= 1)
+                break;
+        }
+
+    }
+
+    if(numPlayersAlive <= 1)
+        cout << "There are not enough players alive to continue. The game is now over" << endl;
+
+    cout << "Thank you for playing Jumanji!" << endl;
 
     return 0;
 }
